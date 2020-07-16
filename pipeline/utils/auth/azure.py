0541a711c86132f60de327324c641759c458151d
William Hearn <sylus1984@gmail.com>
2020-07-16 10:20:43 -0400

    feat(lint): Minor nits

--- pipeline/utils/auth/azure.py
+++ pipeline/utils/auth/azure.py
@@ -4,9 +4,7 @@ import adal
 def get_access_token(tenant, clientId, client_secret, audience):
     authorityHostUrl = "https://login.microsoftonline.com"
     GRAPH_RESOURCE = audience
-
     authority_url = authorityHostUrl + '/' + tenant
-
     context = adal.AuthenticationContext(authority_url)
     token = context.acquire_token_with_client_credentials(GRAPH_RESOURCE, clientId, client_secret)  # noqa: E501
     return token['accessToken']
