b6a5a491396277fd66e193c4564f05199974a92e
Andrew Scribner <ca.scribner+1@gmail.com>
2020-08-06 11:07:08 -0400

    Stage average pipeline

---
+++ scribner/pipeline/build_pipeline_average.py
@@ -0,0 +1,78 @@
+# Based on https://github.com/StatCan/jupyter-notebooks/blob/master/kfp-basics/average_with_docker_components.ipynb
+
+import kfp
+from kfp import dsl
+from kfp import compiler
+import time
+
+
+def average_op(*numbers):
+    """
+    Factory for average ContainerOps
+    
+    Accepts an arbitrary number of input numbers, returning a ContainerOp that passes those
+    numbers to the underlying docker image for averaging
+    
+    Returns output collected from ./out.txt from inside the container
+
+    """
+    # Input validation
+    if len(numbers) < 1:
+        raise ValueError("Must specify at least one number to take the average of")
+        
+    return dsl.ContainerOp(
+        name="average",  # What will show up on the pipeline viewer
+        image="k8scc01covidacr.azurecr.io/kfp-components/average:v1",  # The image that KFP runs to do the work
+        arguments=numbers,  # Passes each number as a separate (string) command line argument
+        # Script inside container writes the result (as a string) to out.txt, which 
+        # KFP reads for us and brings back here as a string
+        file_outputs={'data': './out.txt'},  
+    )
+
+
+@dsl.pipeline(
+    name="my pipeline's name"
+)
+def my_pipeline(a, b, c, d, e):
+    """
+    Averaging pipeline which accepts five numbers and does some averaging operations on them
+    """
+    # Compute averages for two groups
+    avg_1 = average_op(a, b, c)
+    avg_2 = average_op(d, e)
+    
+    # Use the results from _1 and _2 to compute an overall average
+    average_result_overall = average_op(avg_1.output, avg_2.output)
+
+
+if __name__ == "__main__":
+    compiler.Compiler().compile(my_pipeline, __file__ + '.tar.gz')
+
+# Leftovers from porting this.  To delete later
+#
+#
+# pipeline_yaml = 'pipeline.yaml.zip'
+# compiler.Compiler().compile(
+#     my_pipeline,
+#     pipeline_yaml
+# )
+# print(f"Exported pipeline definition to {pipeline_yaml}")
+#
+#
+# experiment_name = "averaging-pipeline"
+# client = kfp.Client()
+# exp = client.create_experiment(name=experiment_name)
+# pl_params = {
+#     'a': 5,
+#     'b': 5,
+#     'c': 8,
+#     'd': 10,
+#     'e': 18,
+# }
+#
+# run = client.run_pipeline(
+#     exp.id,  # Run inside the above experiment
+#     experiment_name + '-' + time.strftime("%Y%m%d-%H%M%S"),  # Give our job a name with a timestamp so its unique
+#     pipeline_yaml,  # Pass the .yaml.zip we created above.  This defines the pipeline
+#     params=pl_params  # Pass our parameters we want to run the pipeline with
+# )
---
+++ scribner/pipeline/run_pipeline_average.py
@@ -0,0 +1,115 @@
+import json
+
+import kfp
+import argparse
+from utils.auth.azure import get_access_token
+
+
+def main():
+    # TODO: Update docstring - this has been modified from original
+    # Demonstrating how to access KFP API (run pipeine)
+    # With an authentication token provided by Azure AD
+    #
+    # Usage:
+    # python run.py --kfp_host <kfp_host> --resource_group <resource_group> --workspace <workspace> --pipeline_id <pipeline_id> --run_path <run_path> --experiment_name <experiment_name> --tenant <tenant> --service_principal <Service Principal> --sp_secret <Service Principal Secret> --sp_audience <Audience> --datasets <datasets>  # noqa: E501
+
+    parser = argparse.ArgumentParser("run pipeline")
+
+    parser.add_argument(
+        "--kfp_host",
+        type=str,
+        required=False,
+        default="http://localhost:8080/pipeline",
+        help="KFP endpoint"
+    )
+
+    # parser.add_argument(
+    #     "--resource_group",
+    #     type=str,
+    #     required=True,
+    #     help="Resource Group"
+    # )
+    #
+    # parser.add_argument(
+    #     "--workspace",
+    #     type=str,
+    #     required=True,
+    #     help="AML Workspace"
+    # )
+
+    parser.add_argument(
+        "--pipeline_id",
+        type=str,
+        required=True,
+        help="Pipeline Id"
+    )
+
+    parser.add_argument(
+        "--run_name",
+        type=str,
+        required=True,
+        help="KFP run name "
+    )
+
+    parser.add_argument(
+        "--experiment_name",
+        type=str,
+        required=False,
+        default="Default",
+        help="Kubeflow experiment name "
+    )
+
+    parser.add_argument(
+        "--tenant",
+        type=str,
+        required=True,
+        help="Tenant"
+    )
+
+    parser.add_argument(
+        "--service_principal",
+        type=str,
+        required=True,
+        help="Service Principal"
+    )
+
+    parser.add_argument(
+        "--sp_secret",
+        type=str,
+        required=True,
+        help="Service Principal Secret"
+    )
+
+    parser.add_argument(
+        "--sp_audience",
+        type=str,
+        required=True,
+        help="Service Principal Audience"
+    )
+
+    parser.add_argument(
+        "--pl_args",
+        type=str,
+        required=True,
+        help="Arguments passed to pipeline.  Defined as a JSON string"
+    )
+
+    args = parser.parse_args()
+    token = get_access_token(args.tenant, args.service_principal, args.sp_secret, args.sp_audience)  # noqa: E501
+    client = kfp.Client(host=args.kfp_host, existing_token=token)
+    exp = client.get_experiment(experiment_name=args.experiment_name)  # noqa: E501
+
+    pipeline_params = {}
+    # pipeline_params["resource_group"] = args.resource_group
+    # pipeline_params["workspace"] = args.workspace
+    # pipeline_params["token"] = token
+    pipeline_params = json.loads(args.pl_args)
+
+    client.run_pipeline(exp.id,
+                        job_name=args.run_name,
+                        params=pipeline_params,
+                        pipeline_id=args.pipeline_id)
+
+
+if __name__ == '__main__':
+    exit(main())
