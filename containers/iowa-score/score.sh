fecf18e16f6b10f1b2d793c97e24414a9ffc7fe7
Andrew Scribner <ca.scribner+1@gmail.com>
2020-08-18 12:02:09 -0400

    feat: Add Iowa scoring, run.py, update build (#20)

--- containers/iowa-score/score.sh
+++ containers/iowa-score/score.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
-# Note: Could also do this runner like here:
-# https://github.com/kaizentm/kubemlops/blob/master/code/notebook-comp/src/program.py
-# They run from inside python rather than shell, can then use argparse etc.
-# Maybe make a generic wrapper that passes all args to notebook and checks
-# if they were expected?
-
 # TODO: Make this into a generic papermill wrapper that just blindly accepts 
 # --kwargs and assigns them as -p variables, and also auto-generates the 
 # completed notebook path?  Or just stop caring about returning the completed 
 # notebook?
 
 # Example training call
-# bash train.sh --notebook train.ipynb --output_notebook complete.ipynb --data data_train.csv --output_model model.joblib --svm_gamma 0.1 --svm_c 100
+# bash score.sh --notebook score.ipynb --output_notebook complete.ipynb --data test.csv --model model.joblib --output_classification_report_filename classification_report.csv --output_f1_filename f1.txt
 
 PARAMS=""
 while (( "$#" )); do
@@ -53,18 +47,27 @@ while (( "$#" )); do
         exit 1
       fi
       ;;
-    --svm_gamma)
+    --model)
       if [ -n "$2" ] && [ ${2:0:1} != "-" ]; then
-        SVM_GAMMA=$2
+        MODEL=$2
         shift 2
       else
         echo "Error: Argument for $1 is missing" >&2
         exit 1
       fi
       ;;
-    --svm_c)
+    --output_classification_report_filename)
       if [ -n "$2" ] && [ ${2:0:1} != "-" ]; then
-        SVM_C=$2
+        OUTPUT_CLASSIFICATION_REPORT_FILENAME=$2
+        shift 2
+      else
+        echo "Error: Argument for $1 is missing" >&2
+        exit 1
+      fi
+      ;;
+    --output_f1_filename)
+      if [ -n "$2" ] && [ ${2:0:1} != "-" ]; then
+        OUTPUT_F1_FILENAME=$2
         shift 2
       else
         echo "Error: Argument for $1 is missing" >&2
@@ -87,8 +90,9 @@ done
 # KFP may pass filenames with parent directories that do not yet exist
 # Create directories for output files
 mkdir -p `dirname $OUTPUT_NOTEBOOK`
-mkdir -p `dirname $OUTPUT_MODEL`
+mkdir -p `dirname $OUTPUT_CLASSIFICATION_REPORT_FILENAME`
+mkdir -p `dirname $OUTPUT_F1_FILENAME`
 
-cmd="papermill $NOTEBOOK $OUTPUT_NOTEBOOK -p data $DATA -p output_model $OUTPUT_MODEL -p svm_gamma $SVM_GAMMA -p svm_C $SVM_C"
+cmd="papermill $NOTEBOOK $OUTPUT_NOTEBOOK -p data $DATA -p model $MODEL -p output_classification_report_filename $OUTPUT_CLASSIFICATION_REPORT_FILENAME -p output_f1_filename $OUTPUT_F1_FILENAME"
 echo Executing: $cmd
 eval $cmd
