69f971dee8a75177581ff1cfb9e0c1372958eccc
Andrew Scribner <ca.scribner+1@gmail.com>
2020-08-24 19:59:39 +0000

    feat: continue implementing iowa-train paramfile

--- containers/iowa-train/train.sh
+++ containers/iowa-train/train.sh
@@ -12,7 +12,7 @@
 # notebook?
 
 # Example training call
-# bash train.sh --notebook train.ipynb --output_notebook complete.ipynb --data data_train.csv --output_model model.joblib --svm_gamma 0.1 --svm_c 100
+# bash train.sh --notebook train.ipynb --output_notebook complete.ipynb --output_params params_output.yml --data data_train.csv --output_model model.joblib --svm_gamma 0.1 --svm_c 100
 
 PARAMS=""
 while (( "$#" )); do
@@ -38,6 +38,15 @@ while (( "$#" )); do
     --output_model)
       if [ -n "$2" ] && [ ${2:0:1} != "-" ]; then
         OUTPUT_MODEL=$2
+        shift 2
+      else
+        echo "Error: Argument for $1 is missing" >&2
+        exit 1
+      fi
+      ;;
+    --output_params)
+      if [ -n "$2" ] && [ ${2:0:1} != "-" ]; then
+        OUTPUT_PARAMS=$2
         shift 2
       else
         echo "Error: Argument for $1 is missing" >&2
@@ -89,6 +98,6 @@ done
 mkdir -p `dirname $OUTPUT_NOTEBOOK`
 mkdir -p `dirname $OUTPUT_MODEL`
 
-cmd="papermill $NOTEBOOK $OUTPUT_NOTEBOOK -p data $DATA -p output_model $OUTPUT_MODEL -p svm_gamma $SVM_GAMMA -p svm_C $SVM_C"
+cmd="papermill $NOTEBOOK $OUTPUT_NOTEBOOK -p data $DATA -p output_model $OUTPUT_MODEL -p output_params $OUTPUT_PARAMS -p svm_gamma $SVM_GAMMA -p svm_C $SVM_C"
 echo Executing: $cmd
 eval $cmd
