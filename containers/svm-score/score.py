4a06c881f2cb46bd0b78a04e3e2375b0891ce0f4
chritter <christian.ritter@canada.ca>
2020-09-08 18:58:08 -0400

    outside

--- containers/svm-score/score.py
+++ containers/svm-score/score.py
@@ -11,12 +11,12 @@ import nltk
 application = Flask(__name__)
 
 model = None
+nltk.download('punkt', download_dir='/tmp')
 
 
 def load_model():
     global model
     if (model is None):
-        nltk.download('punkt', download_dir='/tmp')
         print('Attempting to load model')
        
         model = joblib.load('/app/model.pkl')
