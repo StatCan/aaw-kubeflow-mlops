ae0c817bf18edef37d9198068f27800a32f162f4
William Hearn <sylus1984@gmail.com>
2020-12-28 22:34:22 -0500

    feat(ci): Change experiment name

--- pipeline/train/cnn.py
+++ pipeline/train/cnn.py
@@ -163,7 +163,7 @@ def cnn_train(
                 '/scripts/register.py',
                 '--model', 'model',
                 '--model_name', model_name,
-                '--experiment_name', 'mlops',
+                '--experiment_name', 'kubeflow-mlops',
                 '--run_id', dsl.RUN_ID_PLACEHOLDER
             ]
         ).apply(use_azure_secret()).add_env_variable(V1EnvVar(name="MLFLOW_TRACKING_URI", value=mlflow_url)).add_env_variable(V1EnvVar(name="MLFLOW_TRACKING_TOKEN", value=token))  # noqa: E501
